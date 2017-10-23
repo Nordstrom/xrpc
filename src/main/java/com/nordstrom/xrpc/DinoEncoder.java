ef36088fcc6aaa8a43f83ea427b1d648a66b2c74
Jesse Kinkead <jesse.kinkead@nordstrom.com>
2017-10-23 12:38:43 -0700

    Use proto3 google compiler.

--- src/main/java/com/nordstrom/xrpc/DinoEncoder.java
+++ src/main/java/com/nordstrom/xrpc/DinoEncoder.java
@@ -16,19 +16,14 @@
 
 package com.nordstrom.xrpc;
 
+import com.nordstrom.xrpc.proto.Dino;
+
 import java.io.IOException;
 
 public class DinoEncoder {
 
   public static void main(String[] args) throws IOException {
-
-    Dino dino = new Dino.Builder()
-      .name(args[0])
-      .fav_color(args[1])
-      .build();
-
-    byte[] bytes = Dino.ADAPTER.encode(dino);
-    System.out.write(bytes, 0, bytes.length);
+    Dino dino = Dino.newBuilder().setName(args[0]).setFavColor(args[1]).build();
+    System.out.write(dino.toByteArray());
   }
-
 }
