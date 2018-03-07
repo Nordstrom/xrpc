4841c434580624cf592d9dd314bf8270b1cf376e
Andy Day <andy.day@nordstrom.com>
2018-03-07 13:45:56 -0800

    wip: exception handling

--- xrpc/src/main/java/com/nordstrom/xrpc/server/ResponseFactory.java
+++ xrpc/src/main/java/com/nordstrom/xrpc/server/ResponseFactory.java
@@ -4,6 +4,8 @@ import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
 import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
 
 import com.nordstrom.xrpc.encoding.Encoder;
+import com.nordstrom.xrpc.exceptions.HttpResponseException;
+import com.nordstrom.xrpc.exceptions.InternalServerErrorException;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
@@ -14,8 +16,12 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import java.io.IOException;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface ResponseFactory {
+  Logger log = LoggerFactory.getLogger(ResponseFactory.class);
+
   XrpcRequest request();
 
   default HttpResponse ok() {
@@ -45,6 +51,27 @@ public interface ResponseFactory {
 
   default <T> HttpResponse internalServerError(T body) throws IOException {
     return createResponse(HttpResponseStatus.INTERNAL_SERVER_ERROR, body);
+  }
+
+  default HttpResponse exception(Exception exception) {
+    try {
+      // TODO (AD): Use configured response content/type and response specific exceptions to default
+      // to meaningful responses here
+      log.error("Handler Exception:", exception);
+
+      if (exception instanceof HttpResponseException) {
+        HttpResponseException responseException = (HttpResponseException) exception;
+        return createResponse(
+            HttpResponseStatus.valueOf(responseException.getStatusCode()), responseException);
+      }
+      // TODO (AD): Handle other exceptions that can reasonably be converted to HTTP Responses.
+      // For example IllegalArgumentException could become HTTP 400 Bad Request
+    } catch (Exception e) {
+      log.error("Error Handling Exception:", e);
+    }
+    return createResponse(
+        HttpResponseStatus.INTERNAL_SERVER_ERROR,
+        new InternalServerErrorException("Internal Server Error"));
   }
 
   default <T> HttpResponse createResponse(HttpResponseStatus status, T body) throws IOException {
